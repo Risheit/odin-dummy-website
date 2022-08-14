31f0851b2a3b90afe8566e7cbaac4b795b918bef
Risheit <80658104+Risheit@users.noreply.github.com>
2022-08-13 20:54:24 -0500

    Add dynamic password mismatch message

--- scripts/signup.js
+++ scripts/signup.js
@@ -3,14 +3,37 @@ function checkPassword() {
     const confirmation = document.querySelector("#confirm_password");
 
     if ((password.value.length !== 0 || confirmation.value.length !== 0)
-        && password.value !== confirmation.value) {
+    && password.value !== confirmation.value) {
         const invalidString = "Passwords don't match.";
+ 
         password.setCustomValidity(invalidString);
         confirmation.setCustomValidity(invalidString);
+
+        createErrorMessage();
+
     } else if (password.value.length !== 0 && confirmation.value.length !== 0) {
         password.setCustomValidity("");
         confirmation.setCustomValidity("");
+
+        removeErrorMessage();
     }
+}
+
+function createErrorMessage() {
+    if (document.querySelector("#password_mismatch") !== null)
+        return;
+    
+    const message = document.createElement("label");
+
+    message.id = "password_mismatch";
+    message.textContent = "* Passwords do not match"
+
+    const passwordElement = document.querySelector("#password");
+    passwordElement.parentNode.appendChild(message);
+}
+
+function removeErrorMessage() {
+    document.querySelector("#password_mismatch").remove();
 }
 
 setInterval(checkPassword);
